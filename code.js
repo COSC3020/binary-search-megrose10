3f5ecca5623b32770c86e393a763fc15f4b05667
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-11-07 19:46:04 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,25 @@
 function binarySearch(list, element) {
-    let topList = list.length - 1;
-    let bottomList = 0;
-    while(topList >= bottomList) {
-      let halfList = Math.floor((bottomList + topList)/2)
-      if(element == list[halfList]) {
-        return halfList + 1;
-      }
-      else if(element > list[halfList]) {
-        bottomList = halfList + 1;
-      }
-      else if(element < list[halfList]) {
-        topList = halfList - 1;
-      }
+    if(list.length == 0) {
+        return -1;
+    }
+    else if(list[0] == element) {
+        return 0;
+    }
+    else {
+        let topList = list.length - 1;
+        let bottomList = 0;
+        while(topList >= bottomList) {
+          let halfList = Math.floor((bottomList + topList)/2)
+          if(element == list[halfList]) {
+            return halfList + 1;
+          }
+          else if(element > list[halfList]) {
+            bottomList = halfList + 1;
+          }
+          else if(element < list[halfList]) {
+            topList = halfList - 1;
+          }
+        }
     }
     
     return -1;
